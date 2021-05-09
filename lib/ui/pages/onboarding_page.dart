15f0026350ae4a18bdc580bbe0646f93dde4bd3e
frozenparadox99 <frostbitecryengine300@gmail.com>
2021-05-09 14:20:50 +0530

    added: onboarding pages setup

---
+++ lib/ui/pages/onboarding_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({Key key}) : super(key: key);
+
+  @override
+  _OnboardingPageState createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  var pages = [
+    PageViewModel(
+        title: "Title of the first page",
+        body:
+        "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.asset("assets/onboarding-image-1.png", height: 175.0),
+        ),
+        footer: Image.asset(
+          'assets/waterkard.png',
+          height: 100.0,
+          fit: BoxFit.cover,
+        ),
+        decoration: PageDecoration(
+          pageColor: Color(0xFFFFFFFF),
+          bodyTextStyle: TextStyle(color: Colors.black, fontSize: 16),
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 24),
+          imagePadding: EdgeInsets.zero,
+        )),
+    PageViewModel(
+        title: "Title of the second page",
+        body:
+        "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.asset("assets/onboarding-image-2.jpg", height: 175.0),
+        ),
+        footer: Image.asset(
+          'assets/waterkard.png',
+          height: 100.0,
+          fit: BoxFit.cover,
+        ),
+        decoration: PageDecoration(
+          pageColor: Color(0xFFFFFFFF),
+          bodyTextStyle: TextStyle(color: Colors.black, fontSize: 16),
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 24),
+          imagePadding: EdgeInsets.zero,
+        )),
+    PageViewModel(
+        title: "Title of the third page",
+        body:
+        "Here you can write the description of the page, to explain someting...",
+        image: Center(
+          child: Image.asset("assets/onboarding-image-3.jpg", height: 175.0),
+        ),
+        footer: Image.asset(
+          'assets/waterkard.png',
+          height: 100.0,
+          fit: BoxFit.cover,
+        ),
+        decoration: PageDecoration(
+          pageColor: Color(0xFFFFFFFF),
+          bodyTextStyle: TextStyle(color: Colors.black, fontSize: 16),
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 24),
+          imagePadding: EdgeInsets.zero,
+        )),
+  ];
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      body: IntroductionScreen(
+
+        pages: pages,
+        onDone: () {
+          // When done button is press
+          print('Done Pressed');
+        },
+        onSkip: () {
+          // You can also override onSkip callback
+          print('Skip Pressed');
+        },
+        showSkipButton: true,
+        skip: const Text('Skip',style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black)),
+        next: Icon(
+          Icons.arrow_right,
+          color: Colors.black,
+          size: 30.0,
+        ),
+        done: Text("Done",
+            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black)),
+        dotsDecorator: DotsDecorator(
+            size: Size.square(10.0),
+            activeSize: Size(20.0, 10.0),
+            activeColor: Colors.deepOrange,
+            color: Colors.black26,
+            spacing: EdgeInsets.symmetric(horizontal: 3.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25.0))),
+      ),
+    );
+  }
+}
